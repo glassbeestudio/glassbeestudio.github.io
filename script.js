98447da89b691ef75ae0a1ac3c6423d791f547da
glassbeestudio <glassbeestudio@gmail.com>
2025-08-30 17:21:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+document.querySelectorAll('.lightbox').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
 
+    const overlay = document.createElement('div');
+    overlay.className = 'lightbox-overlay';
+
+    const img = document.createElement('img');
+    img.src = this.href;
+
+    overlay.appendChild(img);
+    document.body.appendChild(overlay);
+
+    overlay.addEventListener('click', () => {
+      overlay.remove();
+    });
+  });
+});
